cd2f4c2ae73c4a99e0b1c784ed18245198bd843a
Ali Bhimani <ali_bhimani21@aol.com>
2020-06-17 12:57:06 +0100

    search working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import 'bulma'
 import './styles/style.scss'
 
-const App = () => (
-  <h1>Hello Router!</h1>
-)
+const App = () => {
+  const [text, setText] = useState('')
+  const [fetchedData, updateFetchedData] = useState([])
+  const search = ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=' + `${text}`)
 
+  function getFetch() {
+    fetch(search)
+      .then(resp => resp.json())
+      .then(data => {
+        updateFetchedData(data)
+        console.log(data)
+      })
+  }
+
+  return <div>
+    <input
+      type="text"
+      onChange={(event) => setText(event.target.value)}
+      value={text}
+    />
+
+
+    <button onClick={() =>
+      getFetch()
+    } >
+      Search
+    </button>
+
+  </div >
+
+
+
+
+}
+
+// fetch ('deezer.com/search/artist?q=`${(target.value)}`')
 export default App
